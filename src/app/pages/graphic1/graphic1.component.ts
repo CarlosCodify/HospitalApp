3002c5a5f0bed8492e8b5bddc42be10042c3a031
carloscodify <carlos.gutierrez@codify.com.bo>
2024-01-25 12:17:14 -0400

    first commit

---
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphic1',
+  templateUrl: './graphic1.component.html',
+  styleUrl: './graphic1.component.css'
+})
+export class Graphic1Component {
+
+}
